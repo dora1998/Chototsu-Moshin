7e5ab443c6f3d853a2bae4127956f9b8e2a1d77e
dora1998 <dorayaki.app@gmail.com>
2018-12-31 22:24:14 +0900

    初期設定

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+// [定数] webpack の出力オプションを指定します
+// 'production' か 'development' を指定
+const MODE = 'development';
+ 
+// ソースマップの利用有無(productionのときはソースマップを利用しない)
+const enabledSourceMap = (MODE === 'development');
+ 
+module.exports = {
+  // モード値を production に設定すると最適化された状態で、
+  // development に設定するとソースマップ有効でJSファイルが出力される
+  mode: MODE,
+ 
+  module: {
+    rules: [
+      // Sassファイルの読み込みとコンパイル
+      {
+        test: /\.scss/, // 対象となるファイルの拡張子
+        use: [
+          // linkタグに出力する機能
+          'style-loader',
+          // CSSをバンドルするための機能
+          {
+            loader: 'css-loader',
+            options: {
+              url: false,
+              sourceMap: enabledSourceMap,
+              // minimize: true,
+ 
+              // 0 => no loaders (default);
+              // 1 => postcss-loader;
+              // 2 => postcss-loader, sass-loader
+              importLoaders: 2
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: enabledSourceMap,
+              plugins: [
+                // ベンダープレフィックスを付与
+                require('autoprefixer')({grid: true})
+              ]
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              // ソースマップの利用有無
+              sourceMap: enabledSourceMap,
+            }
+          }
+        ],
+      },
+    ],
+  },
+};
